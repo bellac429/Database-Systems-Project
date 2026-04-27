26d14a980b7a38886ba5c31943ca81ab79081983
yongycheng <yongycheng@uiowa.edu>
2026-04-27 16:28:35 -0500

    created pages and used router for website structure.

---
+++ frontend/src/pages/JobListing.jsx
@@ -0,0 +1,11 @@
+import './JobListing.css'
+
+function JobListing() {
+        return(
+                <>
+                        <h1>JobListing</h1>
+                </>
+        )
+}
+
+export default JobListing
