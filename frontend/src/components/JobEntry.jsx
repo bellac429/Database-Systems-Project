6675531d7720050cdb51eb7bf9d56bfa315acfb5
yongycheng <yongycheng@uiowa.edu>
2026-04-30 17:30:26 -0500

    feed renders listing content

--- frontend/src/components/JobEntry.jsx
+++ frontend/src/components/JobEntry.jsx
@@ -1,21 +1,23 @@
 import './JobEntry.css'
 
-function JobEntry() {
+function JobEntry({companyName, postDate, description, externalLink, dateDue}) {
         return(
                 <div className='jobentry-container'>
                         <div className='jobentry-company'>
                                 <div className='jobentry-company-icon'></div>
                                 <div className='jobentry-company-name'>
-                                        <h1>Company name</h1>
+                                        <h1>{companyName}</h1>
                                 </div>
                         </div>
-                        <h1 className='jobentry-title'>Job Name</h1>
+                        <h1 className='jobentry-posttime'>posted on {postDate}</h1>
                         <div className='jobentry-info-container'>
                                 <h1 className='jobentry-title'>Description:</h1>
-                                <p>Seeking a motivated Entry-Level Software Engineer to support the design, development, testing, and maintenance of software applications under senior guidance. Write clean, efficient, and well-documented code Assist in developing new features Debug and resolve issues in existing systems Collaborate with cross-functional teams. Learn and apply new technologies and best practices.</p>
+                                <p>{description}</p>
+                                <h1 className='jobentry-title'>Apply Externally:</h1>
+                                <p>{externalLink}</p>
                         </div>
                         <div className='jobentry-apply'>
-                                <h3>Apply by: timestamp</h3>
+                                <h3>Apply by: {dateDue}</h3>
                                 <button>Apply</button>
                         </div>
                 </div>
